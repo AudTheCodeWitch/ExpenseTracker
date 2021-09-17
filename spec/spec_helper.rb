d4685293768b68b3f809e3963d97de6f8698bacd
Audrea Cook <audrea@codewitch.dev>
2021-09-17 15:45:04 -0600

    Filter gems from backtrace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,8 +46,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
@@ -79,7 +79,7 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = "doc"
+    config.default_formatter = 'doc'
   end
 
   # Print the 10 slowest examples and example groups at the
@@ -99,4 +99,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   # Kernel.srand config.seed
 
+  config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra'
 end
