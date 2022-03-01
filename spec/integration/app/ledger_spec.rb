86ac53e3b6f4240e35ef35a72a71037d4fbb0298
Audrea Cook <audrea.cook@dutchie.com>
2022-03-01 08:03:24 -0700

    Stub ledger_spec

---
+++ spec/integration/app/ledger_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../../../app/ledger'
+require_relative '../../../config/sequel'
+require_relative '../../support/db'
+
+module ExpenseTracker
+  RSpec.describe Ledger do
+    let(:ledger) { Ledger.new }
+    let(:expense) do
+      {
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2017-06-10'
+      }
+    end
+
+    describe '#record' do
+      # Contexts go here
+    end
+  end
+end
